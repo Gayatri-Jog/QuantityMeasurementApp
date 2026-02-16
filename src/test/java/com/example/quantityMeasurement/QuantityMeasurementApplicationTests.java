d6acbb911a503dd86edf230a1db1e11bed6428ad
Gayatri-Jog <gayatrinjog@gmail.com>
2026-02-16 14:23:07 +0530

    UC3 JUnit test cases

--- src/test/java/com/example/quantityMeasurement/QuantityMeasurementApplicationTests.java
+++ src/test/java/com/example/quantityMeasurement/QuantityMeasurementApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.quantityMeasurement;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,62 @@ class QuantityMeasurementApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void testFeetEquality_same(){
+		Length feet1 = new Length(1.25, Length.LengthUnit.FEET);
+		Length feet2 = new Length(1.25, Length.LengthUnit.FEET);
+		Assertions.assertTrue(feet1.equals(feet2));
+	}
+
+	@Test
+	public void testFeetEquality_diff(){
+		Length feet1 = new Length(1.2, Length.LengthUnit.FEET);
+		Length feet2 = new Length(0.5, Length.LengthUnit.FEET);
+		Assertions.assertFalse(feet1.equals(feet2));
+	}
+
+	@Test
+	public void testInchesEquality_same(){
+		Length feet1 = new Length(13, Length.LengthUnit.INCHES);
+		Length feet2 = new Length(13, Length.LengthUnit.INCHES);
+		Assertions.assertTrue(feet1.equals(feet2));
+	}
+
+	@Test
+	public void testInchesEquality_diff(){
+		Length feet1 = new Length(12, Length.LengthUnit.INCHES);
+		Length feet2 = new Length(16, Length.LengthUnit.INCHES);
+		Assertions.assertFalse(feet1.equals(feet2));
+	}
+
+	@Test
+	public void testInchesFeetComparison(){
+		Length inches1 = new Length(12, Length.LengthUnit.INCHES);
+		Length feet1 = new Length(1, Length.LengthUnit.FEET);
+		Assertions.assertTrue(inches1.equals(feet1));
+	}
+
+	@Test
+	public void testFeetInchesComparison(){
+		Length feet1 = new Length(2, Length.LengthUnit.FEET);
+		Length inches1 = new Length(24, Length.LengthUnit.INCHES);
+		Assertions.assertTrue(feet1.equals(inches1));
+	}
+
+	@Test
+	public void testFeetInchesComparisonInvalid(){
+		Length feet1 = new Length(24, Length.LengthUnit.FEET);
+		Length inches1 = new Length(24, Length.LengthUnit.INCHES);
+		Assertions.assertFalse(feet1.equals(inches1));
+	}
+
+	@Test
+	public void testInchesEquality_null(){
+		Length feet1 = new Length(12, Length.LengthUnit.INCHES);
+		Length feet2 = null;
+		Assertions.assertFalse(feet1.equals(feet2));
 	}
 
 }
